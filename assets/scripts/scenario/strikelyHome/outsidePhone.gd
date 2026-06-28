49071e831b26c1e0702bc3ec01a7a70e6e46d9f7
VaJau2 <vadim206-1998@yandex.ru>
2026-06-28 17:10:38 +0300

    scene change to lab

--- assets/scripts/scenario/strikelyHome/outsidePhone.gd
+++ assets/scripts/scenario/strikelyHome/outsidePhone.gd
@@ -5,6 +5,7 @@ extends Node
 @export var player: CharacterBody2D
 @export var phone: Sprite2D
 @export var phone_pony: Sprite2D
+@export var next_level: Node
 
 
 func ring() -> void:
@@ -19,3 +20,7 @@ func take() -> void:
 	player.visible = false
 	var movement: MovementController = player.get_node("movement_controller")
 	movement.may_move = false
+
+
+func move_to_lab() -> void:
+	next_level.start()
